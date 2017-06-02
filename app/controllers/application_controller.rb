6e4ce395299817701485b1533ddca1cad6021263
wesrobinson <wes@keystacksolutions.com>
2017-06-02 03:12:38 -0500

    Add domain logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   def set_site_domain
     @site_domain = request.domain
+    @site_domain = "www.#{@site_domain}" if request.subdomain == 'www'
     @site_domain = request.domain + ':' + request.port.to_s if Rails.env.development?
   end
 end
