341ccc41ec0219129c9eae8cbd4eff12380198d9
wesrobinson <wes@keystacksolutions.com>
2017-06-02 03:37:34 -0500

    Fix domain issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :set_site_domain
 
   def set_site_domain
-    @site_domain = request.domain
-    @site_domain = "www.#{@site_domain}"
-    @site_top_domain = @site_domain
-    @site_domain = request.domain + ':' + request.port.to_s if Rails.env.development?
+    @site_top_domain = request.domain
+    @site_domain = "www.#{@site_top_domain}"
+    @site_domain = @site_domain + ':' + request.port.to_s if Rails.env.development?
+    @site_top_domain = @site_top_domain + ':' + request.port.to_s if Rails.env.development?
   end
 end
