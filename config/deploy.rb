c847f28b633d192ddf9d27083ec6296f18307c51
Devery Channell <dtchannell@gmail.com>
2018-01-30 19:56:22 -0600

    Completed prod deploy setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:Keystack/rfq-parts.git"
 
 # Default value for :linked_files is []
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/application.yml', 'config/puma.rb')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/application.yml', 'config/secrets.yml', 'config/puma.rb')
  
 # Default value for linked_dirs is []
 # append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
@@ -26,6 +26,8 @@ set :nginx_config_name, "rfqparts"
 # Puma:
 set :puma_conf, "#{shared_path}/config/puma.rb"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
 
 namespace :deploy do
   before 'check:linked_files', 'puma:config'
